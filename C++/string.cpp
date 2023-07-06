94f9a59177a3f15266f57f7df9363e3a6b738ccc
Rajeev Krishna <lambo2572@gmail.com>
2023-07-06 23:30:26 +0530

    to take whole line input (getline) is used

--- C++/string.cpp
+++ C++/string.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main(){
-    string s = "Rajeev";
-    cout << s[0];
+    // string s = "Rajeev";
+    // cout << s[0];
+    string str;
+    getline (cin, str); // this takes whole line as input
+    // cin >> str;
+    cout << str;
     return 0;
 }
