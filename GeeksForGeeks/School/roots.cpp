14a99b35f5998d3f1ac7089e9c60cb19769aeee4
Rajeev Krishna <lambo2572@gmail.com>
2023-07-18 23:31:37 +0530

    Array reversal, square and cube root math functions

---
+++ GeeksForGeeks/School/roots.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n,m;
+    cout << "Enter the two digits: ";
+    cin >> n >> m;
+    cout << "Square root: " << sqrt(n) << endl;
+    cout << "Cube root: "<< cbrt(m);
+}
